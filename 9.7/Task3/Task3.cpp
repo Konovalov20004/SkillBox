3fa2c5d0766b02a57519dcbed4aa920d4b57ede0
Sergey Konovalov <konovalov20004@gmail.com>
2023-10-01 00:22:28 +0300

    Task4 9

---
+++ 9.7/Task3/Task3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <limits>
+#include <numeric>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str;
+	bool beggin = false, end = false;
+	int count = 0;
+	getline(cin, str);
+
+	for (int i = 0; i <= str.length() - 1; i++) {
+		if (i < str.length() - 1 && str[i] == ' ' && str[i + 1] == ' ') {
+			continue;
+		}
+		if (str[i] != ' ' || (str[i] == ' ' && str[i + 1] != ' ')) {
+			beggin = true;
+		}
+		if ((i == str.length() - 1 && str[i] != ' ') || (str[i] != ' ' && str[i + 1] == ' ')) {
+			end = true;
+		}
+		if (end && beggin) {
+			count++;
+			beggin = false;
+			end = false;
+		}
+	}
+	cout << count;
+}
---
+++ 9.7/Task4/Task4.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <limits>
+#include <numeric>
+#include <string>
+
+using namespace std;
+
+void check(string& var) {
+	while(true)
+	{
+		cin >> var;
+		if (cin.fail() || cin.peek() != '\n' || var.length() > 4) {
+			cerr << "error"<< endl;
+
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max() || '\n');
+		}
+		else {
+			break;
+		}
+	}
+}
+
+int main() {
+	string value{}, var{};
+	int countBull = 0, countCow = 0;
+	check(value);
+	while (true) {
+		check(var);
+		for (int i = 0, x = 0; i < value.length() * value.length(); x++) {
+			if (i == x && value[i] == var[i]) {
+				countBull++;
+			}
+			if (value[i] == var[x]) {
+				countCow++;
+			}
+			if (x == var.length() - 1 && i == value.length() - 1) {
+				break;
+			}
+			else if (x == var.length() - 1) {
+				i++;
+				x = 0;
+			}
+		} cout << "Bull\t" << countBull << endl << "Cow\t" << countCow << endl;
+		countBull = 0;
+		countCow = 0;
+	} 
+}
