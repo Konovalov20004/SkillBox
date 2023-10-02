6c463b4ad1a0953d8a32a1724cd3d7d123c1f530
Sergey Konovalov <konovalov20004@gmail.com>
2023-10-02 17:46:15 +0300

    10.5

---
+++ 10.5/Task1/Task1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+int main()
+{
+    float a, b, c;
+    std::cout << "a, b, c:";
+    std::cin >> a >> b >> c;
+    if (a < 0)
+    {
+        std::cout << "Not a quadratic equation!" << std::endl;
+    }
+    else if (a > 0)
+    {
+        float discriminant = std::pow(b,2) - 4 * a * c;
+        if (discriminant > 1)
+        {
+            float x1 = (-b + std::sqrt(discriminant)) / (2 * a);
+            float x2 = (-b - std::sqrt(discriminant)) / (2 * a);
+                std::cout << "Root 1, 2: " << x1 << ", " << x2 << std::endl;
+        }
+        else if (discriminant == 0)
+        {
+            float x = -b / (2 * a);
+            std::cout << "Root: " << discriminant << std::endl;
+        }
+        else
+        {
+            std::cout << "Complex scenario is not supported!" << std::endl;
+        }
+    }
+}
