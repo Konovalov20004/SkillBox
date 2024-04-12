cf2ef47c6b103d30be299b4248b924da96299c57
Sergey Konovalov <konovalov20004@gmail.com>
2024-04-12 19:42:37 +0300

    Seminar 23 done

---
+++ 23/2/2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#define Winter
+
+
+int main()
+{
+#ifdef Winter
+	std::cout << "Winter" << std::endl;
+#endif
+#ifdef Spring
+	std::cout << "Spring" << std::endl;
+#endif
+#ifdef Summer
+	std::cout << "Summer" << std::endl;
+#endif
+#ifdef Fall
+	std::cout << "Fall" << std::endl;
+#endif
+}
