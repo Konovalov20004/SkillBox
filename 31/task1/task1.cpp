51324e95ccf2a6df53d6cabed3a59ef20b74f377
Konovalov20004 <konovalov20004@gmail.com>
2024-05-26 23:09:40 +0300

    Seminar 31

---
+++ 31/task1/task1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+
+class Toy {
+public:
+    std::string name;
+
+    Toy(std::string _name) : name(_name) {};
+    Toy():name("Ball") {};
+    ~Toy() {
+        std::cout << "Toy " << name << " was dropped " << std::endl;
+    }
+};
+
+class Dog {
+private:
+    std::shared_ptr<Toy> toy;
+    std::string name;
+    int age;
+public:
+
+    Dog(std::string _name, std::shared_ptr<Toy> _toy, int _age) :name(_name),toy(_toy), age(_age) {};
+    Dog() : Dog("Pushok", nullptr , 4) {};
+
+    void getToy(std::shared_ptr<Toy> nameToy) {
+        if (toy == nameToy)
+            std::cout << "I already have this toy" << std::endl;
+        else if (nameToy.use_count() > 2)
+            std::cout << "Another dog is playing with this toy" << std::endl;
+        else
+            toy = nameToy;
+    }
+
+    void dropToy() {
+        if (toy)
+            toy.reset();
+        else
+            std::cout << "Nothing to drop" << std::endl;
+    }
+};
+
+int main()
+{
+    std::shared_ptr<Toy> ball = std::make_shared<Toy>("ball");
+    std::shared_ptr<Toy> bone = std::make_shared<Toy>("bone");
+    std::cout << bone.use_count() << std::endl;
+    std::cout << ball.use_count() << std::endl;
+
+    Dog a;
+    Dog b("Pushok", ball, 10);
+
+    b.dropToy();
+    std::cout << "========" << std::endl;
+    a.getToy(ball);
+    b.getToy(bone);
+    std::cout << bone.use_count() << std::endl;
+    std::cout << ball.use_count() << std::endl;
+
+}
+
---
+++ 31/task2/task2.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+
+class Toy {
+private:
+public:
+    std::string name;
+    Toy(std::string _name): name(_name){}
+    Toy() : name("Sometoy") {};
+
+    ~Toy() {
+        std::cout << "Toy " << name << " was dropped" << std::endl;
+    }
+};
+
+class shared_ptr_toy {
+private:
+    Toy* toy;
+    int count{};
+    int* countPtr = &count;
+public:
+    shared_ptr_toy(std::string _name) {
+        (*countPtr)++;
+        toy = new Toy(_name);
+    }
+
+    shared_ptr_toy(const shared_ptr_toy& other) {
+        toy = other.toy;
+        countPtr = other.countPtr;
+        (*countPtr)++;
+    }
+
+    shared_ptr_toy& operator=(const shared_ptr_toy& other) {
+        if (this == &other)
+            return *this;
+        toy = other.toy;
+        (*countPtr)--;
+        countPtr = other.countPtr;
+        (*countPtr)++;
+        return *this;
+    }
+
+    std::string getToyName() const {
+        if (toy != nullptr)
+            return toy->name;
+        return "Noting";
+    }
+
+    int use_count() const {
+        return *countPtr;
+    }
+
+    void reset() {
+        toy = nullptr;
+        (*countPtr)--;
+    }
+
+    Toy* get() const {
+        return toy;
+    }
+
+    ~shared_ptr_toy() {
+        (*countPtr)--;
+        if(*countPtr <= 0)
+            delete toy;
+    }
+};
+
+shared_ptr_toy make_shared_toy(std::string _name) {
+    shared_ptr_toy a(_name);
+    return a;
+}
+
+int main() {
+    shared_ptr_toy toy_01 = make_shared_toy("ball");
+    shared_ptr_toy toy_02(toy_01);
+    shared_ptr_toy toy_03("duck");
+    std::cout << "=================================================" << std::endl;
+    std::cout << toy_01.getToyName() << " links:" << toy_01.use_count() << "  "
+        << toy_02.getToyName() << " links:" << toy_02.use_count() << "  "
+        << toy_03.getToyName() << " links:" << toy_03.use_count() << std::endl;
+    std::cout << "=================================================" << std::endl;
+    toy_02 = toy_03;
+    std::cout << toy_01.getToyName() << " links:" << toy_01.use_count() << "  "
+        << toy_02.getToyName() << " links:" << toy_02.use_count() << "  "
+        << toy_03.getToyName() << " links:" << toy_03.use_count() << std::endl;
+    std::cout << "=================================================" << std::endl;
+    toy_01.reset();
+    std::cout << toy_01.getToyName() << " links:" << toy_01.use_count() << "  "
+        << toy_02.getToyName() << " links:" << toy_02.use_count() << "  "
+      << toy_03.getToyName() << " links:" << toy_03.use_count() << std::endl;
+    std::cout << "=================================================" << std::endl;
+    return 0;
+}
